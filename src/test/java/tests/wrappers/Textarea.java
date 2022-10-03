111132d3a2b9bd8c16a99acb3987fb4792ef5a16
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-10-04 00:44:58 +0300

    adding Textarea wrapper

---
+++ src/test/java/tests/wrappers/Textarea.java
@@ -0,0 +1,20 @@
+package tests.wrappers;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static java.lang.String.format;
+
+public class Textarea {
+    String label;
+
+    public Textarea(String label) {
+        this.label = label;
+    }
+
+    public void input(String text) {
+/*      $x(format("//label[contains(text(), '%s')]//..//input | //label[contains(text(), '%s')]//..//textarea", label, label)).clear();
+        the string above for cleaning Project code field, because Project field
+        fills automatically by first letters of Project name (use if necessary enter your own project code)
+*/
+        $x(format("//label[contains(text(), '%s')]//..//textarea", label)).sendKeys(text);
+    }
+}
