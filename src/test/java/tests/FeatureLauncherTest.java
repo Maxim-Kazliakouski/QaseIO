11ceb24b89e3d0f83193126f6d763d5f5355d36d
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-10-30 18:57:18 +0300

    adding FeatureLauncherTest for launching through cucumber

---
+++ src/test/java/tests/FeatureLauncherTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {
+                "src/test/resources/features/login.feature",
+                "src/test/resources/features/actionsOnProject.feature",
+                "src/test/resources/features/createTestCase.feature",
+                "src/test/resources/features/createTestPlan.feature"
+        },
+        glue = "steps",
+        plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+                "html:target/cucumber.html",
+                "json:target/cucumber.json"}
+)
+public class FeatureLauncherTest extends AbstractTestNGCucumberTests {
+}
