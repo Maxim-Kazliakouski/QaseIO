56759d7b4b114a94eeb25d6dfcdf8e2c768314ac
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-10-30 19:05:07 +0300

    adding POJO for TestCase Root

---
+++ src/test/java/tests/api/moduls/TestCase/Root.java
@@ -0,0 +1,17 @@
+package tests.api.moduls.TestCase;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Root {
+    public boolean status;
+    public Result result;
+}
