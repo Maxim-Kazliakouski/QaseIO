e5b5e22405c408343c723f2074a4b95cc0289f7f
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-10-30 19:05:55 +0300

    adding POJO for Project Runs

---
+++ src/test/java/tests/api/moduls/Project/Runs.java
@@ -0,0 +1,17 @@
+package tests.api.moduls.Project;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Runs {
+    public int total;
+    public int active;
+}
