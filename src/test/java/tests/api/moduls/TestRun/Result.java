1f2293bb69ac768c3c6153528098fd194c0a5733
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-10-30 19:04:04 +0300

    adding POJO for TestRun Result

---
+++ src/test/java/tests/api/moduls/TestRun/Result.java
@@ -0,0 +1,10 @@
+package tests.api.moduls.TestRun;
+
+import java.util.ArrayList;
+
+public class Result {
+    public int total;
+    public int filtered;
+    public int count;
+    public ArrayList<Entity> entities;
+}
