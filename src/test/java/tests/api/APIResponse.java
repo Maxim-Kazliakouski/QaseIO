185f56b5cbefa217d8fd7daea4fc7f9a613ccc3e
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-11-07 01:33:51 +0300

    adding APIResponse class

---
+++ src/test/java/tests/api/APIResponse.java
@@ -0,0 +1,14 @@
+package tests.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class APIResponse<T> {
+    public Boolean status;
+    public T result;
+}
